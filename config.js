adc4b405288c1fbc48b837c60ef51a025c97c824
Nsima George <georgensima@gmail.com>
2022-08-03 18:24:33 -0400

    Validate transactionSeries and create mining rewards

--- config.js
+++ config.js
@@ -1,15 +1,27 @@
+const GENESIS_DATA = {
+    blockHeaders: {
+      parentHash: '--genesis-parent-hash--',
+      beneficiary: '--genesis-beneficiary--',
+      difficulty: 1,
+      number: 0,
+      timestamp: '--genesis-timestamp--',
+      nonce: 0,
+      transactionsRoot: '--genesis-transactions-root-',
+      stateRoot: '--genesis-state-root--'
+    },
+    transactionSeries: []
+  };
   
-const TOTAL_COINS = 200;
-
-const TRANSACTION_THRESHOLD = 5;
-
-const FIRST_LEADER = "";
-
-const TRANSACTION_FEE = 1;
-
-module.exports = {
-  TOTAL_COINS,
-  TRANSACTION_THRESHOLD,
-  FIRST_LEADER,
-  TRANSACTION_FEE
-};
+  const MILLISECONDS = 1;
+  const SECONDS = 1000 * MILLISECONDS;
+  const MINE_RATE = 13 * SECONDS;
+  
+  const STARTING_BALANCE = 1000;
+  const MINING_REWARD = 50;
+  
+  module.exports = {
+    GENESIS_DATA,
+    MINE_RATE,
+    STARTING_BALANCE,
+    MINING_REWARD
+  };
