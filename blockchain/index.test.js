4c3127a48f39fcff897adef6d3b74b3e68879f18
Nsima George <georgensima@gmail.com>
2022-08-03 18:54:29 -0400

    Validate transaction series within blocks

---
+++ blockchain/index.test.js
@@ -0,0 +1,21 @@
+const Block = require("./index");
+const Wallet = require("../wallet/wallet");
+let secret = "i am the first leader";
+describe("Block", () => {
+  let data, lastBlock, block;
+  let wallet;
+  beforeEach(() => {
+    data = "bar";
+    lastBlock = Block.genesis();
+    wallet = new Wallet(secret);
+    block = Block.createBlock(lastBlock, data, wallet);
+  });
+
+  it("sets the data to match the input", () => {
+    expect(block.data[0]).toEqual(data);
+  });
+
+  it("sets the `lastHash` to match the hash of the last block", () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+});
